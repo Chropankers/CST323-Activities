726f37feaf1d39e63cc020716fa656b2d80cd863
Chropankers <51174981+Chropankers@users.noreply.github.com>
2024-11-17 20:38:56 -0500

    Update HomeController.java

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,13 +1,19 @@
 package com.example.demo.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     @GetMapping("/home")
     public String home() {
-        return "index"; // This assumes you have a Thymeleaf template named home.html
+        logger.info("Entering home method");
+        logger.info("Exiting home method");
+        return "index";
     }
 }
