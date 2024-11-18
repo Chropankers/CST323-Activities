830fa6d9c1f2b34f421b6a4b981fc22bb297fe7b
Chropankers <51174981+Chropankers@users.noreply.github.com>
2024-11-17 20:39:58 -0500

    Update SuccessController.java

--- src/main/java/com/example/demo/controllers/SuccessController.java
+++ src/main/java/com/example/demo/controllers/SuccessController.java
@@ -1,13 +1,19 @@
 package com.example.demo.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SuccessController {
 
+    private static final Logger logger = LoggerFactory.getLogger(SuccessController.class);
+
     @GetMapping("/success")
     public String showSuccessPage() {
-        return "success"; // This should correspond to a success.html template
+        logger.info("Entering showSuccessPage method");
+        logger.info("Exiting showSuccessPage method");
+        return "success";
     }
 }
