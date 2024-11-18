d47d0f7aebcb3eb5630b5e5e19c526c5f76b1f1c
Chropankers <51174981+Chropankers@users.noreply.github.com>
2024-11-17 20:38:43 -0500

    Update CustomErrorController.java

--- src/main/java/com/example/demo/controllers/CustomErrorController.java
+++ src/main/java/com/example/demo/controllers/CustomErrorController.java
@@ -1,4 +1,7 @@
 package com.example.demo.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +10,13 @@ import org.springframework.ui.Model;
 @Controller
 public class CustomErrorController implements ErrorController {
 
+    private static final Logger logger = LoggerFactory.getLogger(CustomErrorController.class);
+
     @RequestMapping("/error")
     public String handleError(Model model) {
+        logger.info("Entering handleError method");
         model.addAttribute("message", "The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.");
+        logger.info("Exiting handleError method");
         return "error";
     }
 
